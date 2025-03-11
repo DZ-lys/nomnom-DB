8efa3fe9c553fd62f6b463a6e64f5860d92f9777
24LP2437 24LP2437 <24LP2437@G304-CC0216.local>
2025-03-11 11:24:54 +0800

    cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import { configDotenv } from "dotenv";
 import { connectMongoDb } from "./data-base/mongo-db";
 import categoryRoute from "./routes/food-category.route";
+import cors from "cors";
 
 configDotenv();
 connectMongoDb();
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/addCategory", categoryRoute);
 
